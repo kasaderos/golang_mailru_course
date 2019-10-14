89ab3839cc4c7d507f42d0d4d9ecea7d947380d3
Kaldarov <kb.kaldarov@yandex.ru>
2019-10-14 03:25:04 +0300

    multihash synchronized

--- 2/99_hw/signer/signer.go
+++ 2/99_hw/signer/signer.go
@@ -41,31 +41,55 @@ func SingleHash(in, out chan interface{}) {
 		chans = append(chans, make(chan string))
 	}
 	k := 0
+	ch1 := make(chan interface{}, len(strs)*2)
+	ch2 := make(chan interface{}, len(strs))
+	ch3 := make(chan interface{}, len(strs))
+	//wg := &sync.WaitGroup{}
 	for i, s := range strs {
-		go func(i int, s string, res chan string) {
+		go func(i int, s string, res chan string, k int) {
 			defer func() {
+				//wg.Done()
 				fmt.Println("SingleHash th done!", i, "-", "1")
 			}()
+			<-ch2
 			fmt.Println("SingleHash th ", i, "-", 1)
 			res <- DataSignerCrc32(s)
-		}(i, s, chans[k])
-		go func(i int, s string, out chan string) {
+		}(i, s, chans[k], k)
+
+		//wg.Add(1)
+		go func(i int, s string, out chan string, k int) {
 			defer func() {
+				//wg.Done()
 				fmt.Println("SingleHash th done!", i, "-", "2")
 			}()
+			<-ch1
 			fmt.Println("SingleHash th ", i, "-", 2)
 			out <- DataSignerMd5(s)
-		}(i, s, chans[k+1])
-		go func(i int, res chan string, in chan string) {
+		}(i, s, chans[k+1], k+1)
+		go func(i int, res chan string, in chan string, k int) {
 			defer func() {
+				//wg.Done()
 				fmt.Println("SingleHash th done!", i, "-", "3")
 			}()
-			fmt.Println("SingleHash th ", i, "-", 3)
+			<-ch3
 			s := <-in
+			fmt.Println("SingleHash th ", i, "-", 3)
 			res <- DataSignerCrc32(s)
-		}(i, chans[k+2], chans[k+1])
+		}(i, chans[k+2], chans[k+1], k+2)
 		k += 3
 	}
+	//close(ch)
+
+	for i := 0; i < len(strs); i++ {
+		go func() {
+			ch1 <- 1
+		}()
+	}
+	for i := 0; i < len(strs); i++ {
+		ch3 <- 1
+		ch2 <- 1
+	}
+
 	for i := 0; i < len(chans); i += 3 {
 		res := <-chans[i] + "~" + <-chans[i+2]
 		out <- res
@@ -83,19 +107,24 @@ func MultiHash(in, out chan interface{}) {
 		chans = append(chans, make(chan string))
 	}
 	k := 0
+	ch := make(chan interface{}, len(strs)*6)
 	for i, s := range strs {
 		for th := 0; th <= 5; th++ {
-			go func(i int, s string, res chan string, th int) {
+			go func(i int, s string, res chan string, th int, k int) {
 				defer func() {
 					fmt.Println("MultiHash th done!", i, "-", th)
 				}()
+				<-ch
 				fmt.Println("MultiHash th ", i, "-", th)
 				res <- DataSignerCrc32(strconv.Itoa(th) + s)
-			}(i, s, chans[k], th)
+			}(i, s, chans[k], th, k)
 			k++
 		}
 	}
 	res := ""
+	for j := 0; j < len(strs)*6; j++ {
+		ch <- 1
+	}
 	for i := 0; i < len(chans); i += 6 {
 		res = ""
 		for j := i; j <= i+5; j++ {
