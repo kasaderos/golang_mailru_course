ec3ce949e46e0521be4289135d5d97dbcb6ebf34
kasaderos <hakaze0000@gmail.com>
2019-10-28 20:05:56 +0600

    new version homework_3 coverage

--- 3/99_hw/coverage/client_test.go
+++ 3/99_hw/coverage/client_test.go
@@ -1,3 +1,220 @@
 package main
 
 // тут писать код тестов
+import (
+	// "io"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestLimitAndOffsetFindUsers(t *testing.T) {
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         "serverUrl",
+	}
+	_, err := client.FindUsers(SearchRequest{
+		Limit: -1,
+	})
+	_, err2 := client.FindUsers(SearchRequest{
+		Offset: -1,
+	})
+	if err == nil && err2 == nil {
+		t.Errorf("limit and offset test fail")
+	}
+}
+
+func TestTimeoutServer(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         ts.URL,
+	}
+	_, err := client.FindUsers(SearchRequest{
+		Limit:      300,
+		Offset:     0,
+		Query:      "long request",
+		OrderField: "Name",
+		OrderBy:    OrderByDesc,
+	})
+
+	if err == nil {
+		t.Errorf("timeout test failed")
+	}
+	ts.Close()
+}
+func TestNilRequest(t *testing.T) {
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         "",
+	}
+	nilReq := SearchRequest{}
+	_, err := client.FindUsers(nilReq)
+	if err == nil {
+		t.Errorf("fail nil SearchRequest")
+	}
+}
+
+func TestBadRequest(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         ts.URL + "/yahoo/",
+	}
+	_, err := client.FindUsers(SearchRequest{
+		Limit:      300,
+		Offset:     0,
+		Query:      "EXOSIS",
+		OrderField: "Name",
+		OrderBy:    OrderByAsIs,
+	})
+	if err == nil {
+		t.Errorf("fail BadRequest")
+	}
+	client.URL = ts.URL
+	_, err2 := client.FindUsers(SearchRequest{
+		Limit:      300,
+		Offset:     0,
+		Query:      "EXOSIS",
+		OrderField: "Bad field",
+		OrderBy:    OrderByAsIs,
+	})
+	if err2 == nil {
+		t.Errorf("fail BadRequest BadOrderField")
+	}
+	client.URL = ts.URL + "/google/"
+	_, err3 := client.FindUsers(SearchRequest{
+		Limit:      300,
+		Offset:     0,
+		Query:      "EXOSIS",
+		OrderField: "Name",
+		OrderBy:    OrderByAsIs,
+	})
+	if err3 == nil {
+		t.Errorf("fail BadRequest unknown error")
+	}
+}
+func TestServerAndAuthError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         ts.URL,
+	}
+	filename = "badxml.xml"
+	_, err := client.FindUsers(SearchRequest{
+		Limit:      300,
+		Offset:     0,
+		Query:      "EXOSIS",
+		OrderField: "Name",
+		OrderBy:    OrderByAsIs,
+	})
+	if err == nil {
+		t.Errorf("fail ServerError")
+	}
+	filename = "dataset.xml"
+	client.AccessToken = "bad token"
+	_, err2 := client.FindUsers(SearchRequest{
+		Limit:      300,
+		Offset:     0,
+		Query:      "EXOSIS",
+		OrderField: "Name",
+		OrderBy:    OrderByAsIs,
+	})
+	if err2 == nil {
+		t.Errorf("fail BadAccessToken test")
+	}
+	ts.Close()
+}
+
+func TestBadJsonResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(GiveBadJsonServer))
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         ts.URL,
+	}
+	_, err := client.FindUsers(SearchRequest{
+		Limit:      25,
+		Offset:     0,
+		Query:      "",
+		OrderField: "Name",
+		OrderBy:    1,
+	})
+	if err == nil {
+		t.Errorf("fail error Unmarshal result json")
+	}
+	ts.Close()
+}
+func TestDataFindUsers(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         ts.URL,
+	}
+	_, err := client.FindUsers(SearchRequest{
+		Limit:      30,
+		Offset:     0,
+		Query:      "male",
+		OrderField: "Name",
+		OrderBy:    OrderByAsIs,
+	})
+	_, err2 := client.FindUsers(SearchRequest{
+		Limit:      10,
+		Offset:     3,
+		Query:      "Dillard",
+		OrderField: "Name",
+		OrderBy:    OrderByAsc,
+	})
+	if err != nil {
+		t.Errorf("NextPage true fail")
+	}
+	if err2 != nil {
+		t.Errorf("Normal test without next page failed")
+	}
+	ts.Close()
+}
+
+func TestServerAtoi(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	searcherParams := url.Values{}
+	searcherParams.Add("limit", "blabla")
+	searcherParams.Add("offset", "0")
+	searcherParams.Add("query", "male")
+	searcherParams.Add("order_field", "Name")
+	searcherParams.Add("order_by", "1")
+	searcherReq, errq := http.NewRequest("GET", ts.URL+"?"+searcherParams.Encode(), nil)
+	searcherReq.Header.Add("AccessToken", "authorization")
+	resp, err := client.Do(searcherReq)
+	if resp.StatusCode != http.StatusBadRequest && err == nil && errq != nil {
+		t.Errorf("fail bad limit test")
+	}
+	searcherParams.Add("offset", "eEe")
+	searcherReq2, errq2 := http.NewRequest("GET", ts.URL+"?"+searcherParams.Encode(), nil)
+	searcherReq.Header.Add("AccessToken", "authorization")
+	resp2, err2 := client.Do(searcherReq2)
+	if resp2.StatusCode != http.StatusBadRequest && err2 == nil && errq2 != nil {
+		t.Errorf("fail bad offset test")
+	}
+	ts.Close()
+}
+
+func TestServerUnMarshal(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	filename = "bad_dataset.xml"
+	client := SearchClient{
+		AccessToken: "authorization",
+		URL:         ts.URL,
+	}
+	_, err := client.FindUsers(SearchRequest{
+		Limit:      30,
+		Offset:     0,
+		Query:      "male",
+		OrderField: "Name",
+		OrderBy:    OrderByAsIs,
+	})
+	if err == nil {
+		fmt.Errorf("fail another xml file")
+	}
+	ts.Close()
+}
--- 3/99_hw/coverage/server.go
+++ 3/99_hw/coverage/server.go
@@ -1,3 +1,177 @@
 package main
 
-// тут писать SearchServer
+// тут писать код тестов
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var filename string = "dataset.xml"
+
+type Row struct {
+	XMLName       xml.Name `xml:"row"`
+	Id            int      `xml:"id"  `
+	Guid          string   `xml:"guid"`
+	IsActive      bool     `xml:"isActive"`
+	Balance       string   `xml:"balance"`
+	Picture       string   `xml:"picture"`
+	Age           int      `xml:"age"`
+	EyeColor      string   `xml:"eyeColor"`
+	First_name    string   `xml:"first_name"`
+	Last_name     string   `xml:"last_name"`
+	Gender        string   `xml:"gender"`
+	Company       string   `xml:"company"`
+	Email         string   `xml:"email"`
+	Phone         string   `xml:"phone"`
+	Address       string   `xml:"address"`
+	About         string   `xml:"about"`
+	Registered    string   `xml:"registere"`
+	FavoriteFruit string   `xml:"favoriteFruit"`
+}
+
+type Root struct {
+	XMLName xml.Name `xml:"root"`
+	Version string   `xml:"version,attr"`
+	Rows    []Row    `xml:"row"`
+}
+
+func isInRow(q string, r Row) (res bool) {
+	if strings.Contains(r.Guid, q) ||
+		strings.Contains(r.Balance, q) ||
+		strings.Contains(r.Picture, q) ||
+		strings.Contains(strconv.Itoa(r.Age), q) ||
+		strings.Contains(r.EyeColor, q) ||
+		strings.Contains(r.First_name, q) ||
+		strings.Contains(r.Last_name, q) ||
+		strings.Contains(r.Company, q) ||
+		strings.Contains(r.Email, q) ||
+		strings.Contains(r.Phone, q) ||
+		strings.Contains(r.Address, q) ||
+		strings.Contains(r.About, q) ||
+		strings.Contains(r.Registered, q) ||
+		strings.Contains(r.FavoriteFruit, q) ||
+		strings.Contains(r.Gender, q) {
+		return true
+	}
+	return false
+}
+
+func sortUsers(users []User, order string) {
+	sort.Slice(users, func(i, j int) bool {
+		if order == "-1" {
+			return users[i].Name > users[j].Name
+		}
+		return users[i].Name < users[j].Name
+	})
+}
+
+func getRows() ([]Row, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("can't open file %v", err)
+	}
+	xmlData, _ := ioutil.ReadAll(file)
+	file.Close()
+	root := &Root{}
+	errunmarshal := xml.Unmarshal(xmlData, root)
+	if errunmarshal != nil {
+		return nil, fmt.Errorf("can't unmarshal %v", errunmarshal)
+	}
+	return root.Rows, nil
+}
+
+func appendUser(resQuery []User, r Row) []User {
+	resQuery = append(resQuery,
+		User{
+			Id:     r.Id,
+			Gender: r.Gender,
+			About:  r.About,
+			Name:   r.First_name + r.Last_name,
+			Age:    r.Age,
+		},
+	)
+	return resQuery
+}
+
+func findQuery(query string) ([]User, error) {
+	var resQuery []User
+	rows, err := getRows()
+	if err != nil {
+		return nil, err
+	}
+	for _, r := range rows {
+		if isInRow(query, r) {
+			resQuery = appendUser(resQuery, r)
+		}
+	}
+	return resQuery, nil
+}
+
+func GiveBadJsonServer(w http.ResponseWriter, r *http.Request) {
+	data := `{"id": 1, "name":"Marshal me please"}`
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(data))
+}
+
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}()
+	if r.Header.Get("AccessToken") != "authorization" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if strings.HasPrefix(r.URL.String(), "/yahoo/") {
+		w.WriteHeader(http.StatusBadRequest)
+		errjson := `{"Error": "error YAHOO"}`
+		io.WriteString(w, errjson)
+		return
+	}
+	if strings.HasPrefix(r.URL.String(), "/google/") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	query := r.URL.Query().Get("query")
+	orderField := r.URL.Query().Get("order_field")
+
+	if query == "long request" {
+		time.Sleep(time.Second) // ищем этот текст
+	}
+	if orderField != "Name" { // только по имени
+		w.WriteHeader(http.StatusBadRequest)
+		errjson := `{"Error": "ErrorBadOrderField"}`
+		io.WriteString(w, errjson)
+		return
+	}
+	order := r.URL.Query().Get("order_by")
+	limit, errconv := strconv.Atoi(r.URL.Query().Get("limit"))
+	offset, errconv2 := strconv.Atoi(r.URL.Query().Get("offset"))
+	if errconv != nil || errconv2 != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	results, errquery := findQuery(query)
+	if errquery != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	sortUsers(results, order)
+	if limit > 25 {
+		results = results[offset:limit]
+	}
+	data, _ := json.Marshal(results)
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(data))
+}
